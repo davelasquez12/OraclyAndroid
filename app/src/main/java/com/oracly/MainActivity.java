328235d8e42cf4c5ad65a28c8d03d239e68ee204
David Velasquez <davelasquez12@gmail.com>
2017-01-14 04:58:25 -0600

    First commit; added theme colors and a toolbar to MainActivity

---
+++ app/src/main/java/com/oracly/MainActivity.java
@@ -0,0 +1,37 @@
+package com.oracly;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+{
+	private Toolbar mToolbar;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mToolbar = (Toolbar) findViewById(R.id.oracly_toolbar);
+		setSupportActionBar(mToolbar);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu_toolbar_main_activity, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		return super.onOptionsItemSelected(item);
+	}
+}
